050887015b23ca8d9d98a5da2c3f7dd5a7038c1c
tonydle <tony@mechatony.com>
2019-06-22 15:25:20 +1000

    Added arduino ROS and Liang's change

--- arduino/ros_droid_control/ros_droid_control.ino
+++ arduino/ros_droid_control/ros_droid_control.ino
@@ -1,48 +1,33 @@
-//#include "DRCDroid.h"
-//#include <ros.h>
-//#include <rosserial_arduino/TurnAngle.h>
-//
-//DRCDroid matlab_droid;
-//
-//ros::NodeHandle nh;
-//using rosserial_arduino::TurnAngle;
-//
-//void turnAngleCallback(const TurnAngle::Request & req, TurnAngle::Response & res){
-//  matlab_droid.steerAngle(req.angle);
-//}
-//
-//ros::ServiceServer<TurnAngle::Request, TurnAngle::Response> turn_angle_svc("steerAndDrive",&turnAngleCallback);
-//
-//void setup() {
-//  nh.initNode();
-//  nh.advertiseService(turn_angle_svc);
-//}
-//
-//void loop() {
-//  nh.spinOnce();
-//}
-
 #include "DRCDroid.h"
+#include <ros.h>
+#include <rosserial_arduino/TurnAngle.h>
+#include <rosserial_arduino/Power.h>
 
-DRCDroid matlab_droid(7, 9);  //servo pin, esc_pin
-float motor_power = 0;
-int esc_value = 1500;
+DRCDroid matlab_droid(5, 7);  //servo pin, esc pin
+
+ros::NodeHandle nh;
+using rosserial_arduino::TurnAngle;
+using rosserial_arduino::Power;
+
+
+void turnAngleCallback(const TurnAngle::Request & req, TurnAngle::Response & res){
+  matlab_droid.steerAngle((float)req.angle);
+}
+
+void powerCallback(const Power::Request & req, Power::Response & res){
+  matlab_droid.setPower(req.power);
+}
+
+ros::ServiceServer<TurnAngle::Request, TurnAngle::Response> turn_angle_svc("/droid/steer",&turnAngleCallback);
+ros::ServiceServer<Power::Request, Power::Response> power_svc("/droid/power",&powerCallback);
 
 void setup() {
   matlab_droid.init();
-  Serial.begin(9600);
+  nh.initNode();
+  nh.advertiseService(turn_angle_svc);
+  nh.advertiseService(power_svc);
 }
 
 void loop() {
-  if(Serial.available() > 0)
-  {
-    motor_power = Serial.parseFloat();
-    while(Serial.available())
-    {
-      Serial.read();
-    }
-    Serial.println(motor_power);
-    esc_value = matlab_droid.setPower(motor_power);
-    Serial.println(esc_value);
-  }
+  nh.spinOnce();
 }
